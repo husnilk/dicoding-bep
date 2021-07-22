b4055749fcc70e174bd825a712b50a2ae2a9e234
Husnil Kamil <husnil.k@gmail.com>
2021-07-22 17:09:01 +0700

    First commit

---
+++ handler.js
@@ -0,0 +1,6 @@
+const addBookHandler = (request, h) => {
+    const { title, tags, body } = request.payload;
+};
+
+
+module.exports = { addBookHandler}
---
+++ route.js
@@ -0,0 +1,12 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: () => {},
+  },
+];
+
+const books = [];
+
+module.exports = routes;
+module.exports = books;
